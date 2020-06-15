5ccacf4af71182b2831482eac1da54977c4f621d
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-15 21:33:35 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -16,7 +16,7 @@ class MusicLibraryController
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
     input = gets.strip.downcase
-    while input != "exit"
+    # while input != "exit"
       case input
     when "list songs"
       list_songs
