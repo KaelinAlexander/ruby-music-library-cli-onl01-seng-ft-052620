ee1372a86919a5358621d7cd7a817d216cc3a946
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-19 17:30:12 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -18,7 +18,7 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
-    get_input
+    input = get_input
   end
 
   def get_input
