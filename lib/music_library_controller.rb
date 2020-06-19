19deb6030f4b028411becb6e77b91c3a9e1c9e44
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-19 18:05:00 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -81,7 +81,7 @@ class MusicLibraryController
     genre = Genre.find_by_name(input)
     if genre
     genre.songs.sort {|x, y| x.name <=> y.name}.each.with_index(1) do |song, index|
-      puts "#{index}. #{song.name} - #{song.artist.name}"
+      puts "#{index}. #{song.artist.name} - #{song.name}"
       end
     end
   end
