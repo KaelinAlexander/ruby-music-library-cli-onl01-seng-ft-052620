a28ce0ea59a54355fe944cf2986ae8b65d6a9c8b
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-19 18:41:28 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -77,7 +77,7 @@ class MusicLibraryController
   end
 
   def list_songs_by_genre
-    puts "Please enter the name of a genre."
+    puts "Please enter the name of a genre:"
     input = gets.strip
     genre = Genre.find_by_name(input)
     if genre
