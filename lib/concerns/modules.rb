6da78fb2e51dcd00161bce5e8edc7b4eed695aa8
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-19 17:40:00 +0000

    Automatically backed up by Learn

--- lib/concerns/modules.rb
+++ lib/concerns/modules.rb
@@ -6,13 +6,9 @@ module Findable
       self.all.find{|song| song.name == name}
   end
 
-  # def self.find_or_create_by_name(name)
-  #   if self.class.all.find {|self.class| self.class.name == name}
-  #     all.find {|self.class| self.class.name == name}
-  #   else
-  #     self.new(name)
-  #   end
-  # end
+  def alphabetized
+    self.all.sort {|x, y| x.name <=> y.name}
+  end
 
   def find_or_create_by_name(name)
     if self.find_by_name(name) == NIL
